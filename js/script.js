27b9b5d24829e7cb72152f6abbeb7c1afbe251d6
Shreeraj-Shivale <66412849+Shreeraj-Shivale@users.noreply.github.com>
2021-08-26 19:03:32 +0530

    a

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ window.addEventListener('keyup', (event) => {
   pressed.push(event.key);
   pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length); // gets us the length by working backwards
   if(pressed.join('').includes(secretCode)) {
-    console.log("DING!");
+    console.log("C0n$01E_ME$$@GES_ARE_FUNNY");
     cornify_add();
   }
   console.log(pressed);
