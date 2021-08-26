2e8ef7143b930c6b7cc37d0502c8f06e91bfecb2
Shreeraj-Shivale <66412849+Shreeraj-Shivale@users.noreply.github.com>
2021-08-26 18:01:19 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const pressed = [];
+const secretCode = 'vishwactf';
+
+window.addEventListener('keyup', (event) => {
+            pressed.push(event.key);
+            pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+            if (pressed.join('').includes(secretCode)) {
+                console.log("sugary treat, I am,... maybe answer is hidden in me!!!!");
+                cornify_add();
+                Processing.env.cookie
+                console.log(pressed);
+            });
