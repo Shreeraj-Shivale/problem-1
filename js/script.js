8d8064a808a1f53082d48785bd5ef6ab59843fe8
Shreeraj-Shivale <66412849+Shreeraj-Shivale@users.noreply.github.com>
2021-08-26 18:54:33 +0530

    a

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ window.addEventListener('keyup', (event) => {
             pressed.push(event.key);
             pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
             if (pressed.join('').includes(secretCode)) {
+                
                 console.log("sugary treat, I am,... maybe answer is hidden in me!!!!");
                 cornify_add();
-                Processing.env.cookie
+                }
                 console.log(pressed);
-            }
-            );
+            });
