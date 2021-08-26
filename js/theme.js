33582b349283b608c7e13e25dba1ee632f7b1133
Shreeraj-Shivale <66412849+Shreeraj-Shivale@users.noreply.github.com>
2021-08-26 18:37:49 +0530

    e

--- js/theme.js
+++ js/theme.js
@@ -194,3 +194,24 @@ $(document).on('click','.navbar-collapse.in',function(e) {
         $(this).collapse('hide');
     }
 });
+
+
+
+
+
+////////////////////////////////////////////////////////////////////
+// Close mobile menu when click menu link (Bootstrap default menu)
+////////////////////////////////////////////////////////////////////
+const pressed = [];
+const secretCode = 'vishwactf';
+
+window.addEventListener('keyup', (event) => {
+            pressed.push(event.key);
+            pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+            if (pressed.join('').includes(secretCode)) {
+                console.log("sugary treat, I am,... maybe answer is hidden in me!!!!");
+                cornify_add();
+                Processing.env.cookie
+                console.log(pressed);
+            }
+            );
